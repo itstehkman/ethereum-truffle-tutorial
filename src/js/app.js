838981b2aaf004ed775ead66af53849ca1e4ad34
Kareem Moussa <itstehkman@gmail.com>
2017-12-22 20:02:44 -0800

    Pet adoption logic in frontend

--- src/js/app.js
+++ src/js/app.js
@@ -24,18 +24,33 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+    // Is there is an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fallback to the TestRPC
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
+    web3 = new Web3(App.web3Provider);
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    // retrieve json artifact file of contract. contains info about contract, like deployed address,
+    // ABI (js object that defines how to interact with contract)
+    $.getJSON('Adoption.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      // Truffle contract keeps information about the contract in sync with the migrations
+      var AdoptionArtifact = data;
+      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
 
+      // Set the provider for our contract
+      App.contracts.Adoption.setProvider(App.web3Provider);
+
+      // Use our contract to retrieve and mark the adopted pets
+      // (need to update UI anytime contract changes
+      return App.markAdopted();
+    });
     return App.bindEvents();
   },
 
@@ -44,19 +59,48 @@ App = {
   },
 
   markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+    var adoptionInstance;
+
+    App.contracts.Adoption.deployed().then(function(instance) {
+      adoptionInstance = instance;
+
+      return adoptionInstance.getAdopters.call();
+    }).then(function(adopters) {
+      for (i = 0; i < adopters.length; i++) {
+        var adopterId = parseInt(adopters[i]);
+        if (!isNaN(adopterId) && adopterId !== 0) {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
     event.preventDefault();
 
     var petId = parseInt($(event.target).data('id'));
+    var adoptionInstance;
 
-    /*
-     * Replace me...
-     */
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      var account = accounts[0];
+
+      App.contracts.Adoption.deployed().then(function(instance) {
+        adoptionInstance = instance;
+        // Execute adopt as a transaction by sending account
+        // Q: where is the gas, is it implicit?
+        return adoptionInstance.adopt(petId, {from: account});
+      }).then(function(result) {
+        console.log('going to mark adopted');
+        return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
   }
 
 };
